740327634e2e3e889089154f37a3aa470c230997
Pedro <hernansaldana52@gmail.com>
2020-06-02 17:00:41 -0500

    App de messenger implementada y funcionando

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,18 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
-from django.conf import settings
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('pages/', include(pages_patterns)),
+    # Paths de profiles
     path('profiles/', include(profiles_patterns)),
-    path('admin/', admin.site.urls),
     # Paths de Auth
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
-
+    # Paths de Messenger
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
